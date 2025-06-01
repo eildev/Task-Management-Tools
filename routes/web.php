10d3abb74824a06112e4a4e64756e6aa897e3d2b
kishormahmud9 <74523493+kishormahmud9@users.noreply.github.com>
2025-06-01 16:52:48 +0600

    change url

--- routes/web.php
+++ routes/web.php
@@ -9,37 +9,42 @@ use App\Http\Controllers\TaskGroupController;
 use App\Http\Controllers\TaskController;
 
 
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render('Home');
 });
+
 
 
 
 Route::middleware('auth')->group(function () {
 
-Route::controller(TaskController::class)->group(function () {
-    Route::get('/tasks', 'index')->name('tasks.index');
-    Route::get('/tasks/create', 'create')->name('tasks.create');
-    Route::post('/tasks', 'store')->name('tasks.store');
-    Route::get('/tasks/{task}', 'show')->name('tasks.show');
-    Route::get('/tasks/{task}/edit', 'edit')->name('tasks.edit');
-    Route::put('/tasks/{task}', 'update')->name('tasks.update');
-    Route::delete('/tasks/{task}', 'destroy')->name('tasks.destroy');
-});
-Route::controller(TaskGroupController::class)->group(function () {
-    Route::get('/task-groups', 'index')->name('task-groups.index');
-    Route::get('/task-groups/create', 'create')->name('task-groups.create');
-    Route::post('/task-groups', 'store')->name('task-groups.store');
-    Route::get('/task-groups/{taskGroup}', 'show')->name('task-groups.show');
-    Route::get('/task-groups/{taskGroup}/edit', 'edit')->name('task-groups.edit');
-    Route::put('/task-groups/{taskGroup}', 'update')->name('task-groups.update');
-    Route::delete('/task-groups/{taskGroup}', 'destroy')->name('task-groups.destroy');
-});
+    Route::controller(TaskController::class)->group(function () {
+        Route::get('/tasks', 'index')->name('tasks.index');
+        Route::get('/tasks/create', 'create')->name('tasks.create');
+        Route::post('/tasks', 'store')->name('tasks.store');
+        Route::get('/tasks/{task}', 'show')->name('tasks.show');
+        Route::get('/tasks/{task}/edit', 'edit')->name('tasks.edit');
+        Route::put('/tasks/{task}', 'update')->name('tasks.update');
+        Route::delete('/tasks/{task}', 'destroy')->name('tasks.destroy');
+    });
+    Route::controller(TaskGroupController::class)->group(function () {
+        Route::get('/task-groups', 'index')->name('task-groups.index');
+        Route::get('/task-groups/create', 'create')->name('task-groups.create');
+        Route::post('/task-groups', 'store')->name('task-groups.store');
+        Route::get('/task-groups/{taskGroup}', 'show')->name('task-groups.show');
+        Route::get('/task-groups/{taskGroup}/edit', 'edit')->name('task-groups.edit');
+        Route::put('/task-groups/{taskGroup}', 'update')->name('task-groups.update');
+        Route::delete('/task-groups/{taskGroup}', 'destroy')->name('task-groups.destroy');
+    });
 });
 
 
@@ -57,4 +62,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
