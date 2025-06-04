4cf7042630d09eec86a62ae038e66e441e2bf571
kishormahmud9 <74523493+kishormahmud9@users.noreply.github.com>
2025-06-04 11:16:08 +0600

    Nothing Major change

--- resources/js/components/TaskGroupAddModal.jsx
+++ resources/js/components/TaskGroupAddModal.jsx
@@ -1,23 +1,133 @@
-
+import { useState } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
 
 const TaskGroupAddModal = ({ show, handleClose }) => {
-    console.log("TaskGroupAddModal rendered, show:", show);
+    const [imagePreview, setImagePreview] = useState("");
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setImagePreview(reader.result);
+            };
+            reader.readAsDataURL(file);
+        } else {
+            setImagePreview("");
+        }
+    };
+
+    const handleTaskGroupDataSave = ({}) => {
+        if (!title || !description || !tag || !date) {
+            alert("Please fill in all required fields.");
+            return;
+        }
+
+        const taskData = {
+            title,
+            description,
+            tag,
+            date,
+            image: imagePreview || null,
+        };
+
+        handleSave(taskData, isEdit);
+        setTitle("");
+        setTag("");
+        setDate("");
+        setDescription("");
+        setImagePreview("");
+    };
     return (
         <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
-                <Modal.Title>
-                    Add New Task Group
-                </Modal.Title>
+                <h6>Add Task Group</h6>
             </Modal.Header>
             <Modal.Body>
-                <h2>This is Body</h2>
+                <Form>
+                    {/* Title */}
+                    <Form.Group className="mb-3" controlId="taskTitle">
+                        <Form.Label>Task Group Name</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Enter Task Title"
+                            // value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            required
+                            name="name"
+                        />
+                    </Form.Group>
+
+                    {/* Start Date */}
+                    <Form.Group className="mb-3" controlId="startDate">
+                        <Form.Label>Start Date</Form.Label>
+                        <Form.Control
+                            type="date"
+                            // value={date}
+                            onChange={(e) => setDate(e.target.value)}
+                            required
+                            name="start_date"
+                        />
+                    </Form.Group>
+
+                    {/* End Date */}
+                    <Form.Group className="mb-3" controlId="startDate">
+                        <Form.Label>End Date</Form.Label>
+                        <Form.Control
+                            type="date"
+                            // value={date}
+                            onChange={(e) => setDate(e.target.value)}
+                            required
+                            name="end_date"
+                        />
+                    </Form.Group>
+
+                    {/* Description */}
+                    <Form.Group className="mb-3" controlId="taskDescription">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control
+                            as="textarea"
+                            rows={3}
+                            placeholder="Write some text"
+                            // value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            required
+                            name="description"
+                        />
+                    </Form.Group>
+
+                    {/* Image */}
+                    <Form.Group className="mb-3" controlId="taskImage">
+                        <Form.Label>
+                            Attachments{" "}
+                            <span className="text-muted">
+                                (Jpg, Png format)
+                            </span>
+                        </Form.Label>
+                        <Form.Control
+                            type="file"
+                            accept="image/*"
+                            onChange={handleImageChange}
+                            name="image"
+                        />
+                        {imagePreview && (
+                            <img
+                                src={imagePreview}
+                                alt="Image_Preview"
+                                style={{
+                                    width: "100%",
+                                    marginTop: "10px",
+                                    display: "block",
+                                }}
+                            />
+                        )}
+                    </Form.Group>
+                </Form>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="danger" onClick={handleClose}>
                     Cancel
                 </Button>
-                <Button variant="primary">
+                <Button variant="primary" onClick={handleTaskGroupDataSave}>
                     Save Changes
                 </Button>
             </Modal.Footer>
